a24705925a92acd933a9a7e8b0b9d49dda2975df
Will <willgarrett64@yahoo.com>
2022-05-18 17:33:34 +0100

    refactor: tidy up server history table items

--- src/components/server/ServerHistory.vue
+++ src/components/server/ServerHistory.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="box">
     <h4>Server history</h4>
-    <div class="hidden mt-4 overflow-hidden border border-gray-300 rounded-lg lg:block">
+    <div class="mt-4 overflow-hidden lg:border lg:border-gray-300 lg:rounded-lg">
       <table class="min-w-full divide-y divide-gray-200">
-        <thead class="tableHead">
+        <thead class="hidden lg:table-header-group tableHead">
           <tr>
-            <th scope="col" class="tableHead__cell">
+            <th scope="col" class="tableHead__cell" width="20%">
               Date
+            </th>
+            <th scope="col" class="tableHead__cell" width="20%">
+              Time
             </th>
             <th scope="col" class="tableHead__cell">
               Event
@@ -14,127 +17,63 @@
             <th scope="col" class="tableHead__cell">
               Status
             </th>
-            <th scope="col" class="tableHead__cell">
-              User
-            </th>
           </tr>
         </thead>
         <tbody class="tableBody">
-          <tr v-for="task in tasks" :key="task._key">
-            <td class="tableBody__cell">
-              <div class="flex items-center">
-                <div class="whitespace-nowrap">
-                  <div class="text-sm">
-                    {{ formatDate(task.created) }}
-                  </div>
-                  <div class="text-xs text-gray-400">
-                    {{ formatTime(task.created) }}
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td class="tableBody__cell">
-              <div class="flex items-center">
-                <div class="">
-                  <div class="text-sm text-gray-900">
-                    {{ formatAction(task.action) }}
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td class="tableBody__cell">
-              <div
-                class="text-xs capitalize"
-                :class="task.status === 'aborted' || task.status === 'failed' || task.status === 'deferred' ? 'text-red' : 'text-green'"
-              >
-                {{ task.status }}
-              </div>
-            </td>
-            <td class="tableBody__cell">
-              <span class="">TBC</span>
-            </td>
-          </tr>
+          <ServerHistoryItem
+            v-for="task in tasks"
+            :task="task"
+            :key="task._key"
+          />
         </tbody>
       </table>
     </div>
-
-      <!-- mobile table view -->
-      <div class="flex flex-col w-full mt-4 border-t border-gray-200 divide-y divide-gray-200 lg:hidden">
-        <div v-for="task in tasks" :key="task._key" class="flex flex-col items-start w-full py-3 space-y-2">
-          <div class="flex items-center justify-between w-full">
-            <div class="text-sm text-gray-900 truncate">
-              {{ formatAction(task.action) }}
-            </div>
-          </div>
-          <div class="flex items-center space-x-4 text-xs">
-            <div class="flex items-center space-x-0.5">
-              <CalendarIcon class="w-3.5 h-3.5 text-gray-500" />
-              <span>{{ formatDate(task.created) }}</span>
-            </div>
-            <div class="flex items-center space-x-0.5">
-              <ClockIcon class="w-3.5 h-3.5 text-gray-500" />
-              <span>{{ formatTime(task.created) }}</span>
-            </div>
-            <div class="flex items-center space-x-0.5">
-              <UserIcon class="w-3.5 h-3.5 text-gray-500" />
-              <span>TBC</span>
-            </div>
-          </div>
-          <div class="flex-shrink-0 text-xs text-green-100 opacity-75">
-            <span class="text-gray-900">Status:</span> {{ task.status }}
-          </div>
-        </div>
-      </div>
-
   </div>
 </template>
 
 <script>
+import ServerHistoryItem from '@/components/server/ServerHistoryItem'
 import moment from 'moment'
-import { CalendarIcon, ClockIcon, UserIcon } from '@heroicons/vue/outline'
+
+moment.locale('en-GB')
 
 export default {
   name: 'ServerHistory',
   components: {
-    CalendarIcon,
-    ClockIcon,
-    UserIcon
+    ServerHistoryItem
   },
-  props: ['tasks'],
-  methods: {
-    formatAction(action) {
-      return action.slice(0, 1).toUpperCase() + action.slice(1)
-    },
-    formatDate(timestamp) {
-      return moment(timestamp).format('LL')
-    },
-    formatTime(timestamp) {
-      return moment(timestamp).format('LTS')
-    }
-  },
-  watch: {
-    $route(to, from) {
-    }
-  }
+  props: ['tasks']
 }
 </script>
 <style scoped>
-  .box {
-    @apply bg-white rounded-lg w-full overflow-auto p-4 md:p-6;
+.box {
+  @apply rounded-lg bg-white w-full overflow-auto p-4 md:p-6;
+}
+.tableHead {
+  @apply border-gray-300 border-b rounded-lg w-full bg-gray-50;
+}
+.tableHead__cell {
+  @apply px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase;
+}
+.tableBody {
+  @apply bg-white divide-y divide-gray-200;
+}
+
+table, tbody {
+  @apply block;
+}
+
+@screen lg {
+  table {
+    @apply table;
   }
-  .tableHead {
-    @apply border-gray-300 border-b rounded-lg w-full bg-gray-50;
+
+  tbody {
+    @apply table-row-group;
   }
-  .tableHead__cell {
-    @apply px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase;
+
+  tr {
+    @apply table-row py-0;
   }
-  .tableBody {
-    @apply bg-white divide-y divide-gray-200;
-  }
-  .tableBody tr {
-    /* @apply hover:bg-gray-50 transition duration-300; */
-  }
-  .tableBody__cell {
-    @apply px-6 py-4 text-gray-500;
-  }
+}
 </style>
---
+++ src/components/server/ServerHistoryItem.vue
@@ -0,0 +1,88 @@
+<template>
+  <tr>
+    <td class="tableBody__cell">
+      <div class="flex flex-row justify-center lg:block">
+        <div><CalendarIcon class="table__icon" /></div>
+        <span>{{ formattedDate }}</span>
+      </div>
+    </td>
+    <td class="tableBody__cell">
+      <div class="flex flex-row justify-center lg:block">
+        <div><ClockIcon class="table__icon" /></div>
+        <span>{{ formattedTime }}</span>
+      </div>
+    </td>
+    <td class="tableBody__cell row-start-1 col-span-3">
+      <span class="capitalize task-action">{{ task.action }}</span>
+    </td>
+    <td class="tableBody__cell row-start-3 col-span-3">
+      <span class="mr-2 lg:hidden">Status:</span>
+      <div
+        class="capitalize"
+        :class="isStatusRed ? 'text-red' : 'text-green'"
+      >
+        <span>{{ task.status }}</span>
+      </div>
+    </td>
+  </tr>
+</template>
+
+<script>
+import moment from 'moment'
+import { CalendarIcon, ClockIcon } from '@heroicons/vue/outline'
+
+export default {
+  name: 'ServerHistoryItem',
+  components: {
+    CalendarIcon,
+    ClockIcon
+  },
+  props: ['task'],
+  computed: {
+    formattedAction() {
+      return this.task.action.slice(0, 1).toUpperCase() + this.task.action.slice(1)
+    },
+    formattedDate() {
+      return moment(this.task.created).format('LL')
+    },
+    formattedTime() {
+      return moment(this.task.created).format('LTS')
+    },
+    isStatusRed() {
+      return this.task.status === 'aborted' || this.task.status === 'failed' || this.task.status === 'deferred'
+    }
+  }
+}
+</script>
+<style scoped>
+tr {
+  @apply grid grid-rows-3 py-2 gap-x-2 gap-y-1;
+  grid-template-columns: auto auto 1fr;
+}
+
+.tableBody__cell {
+  @apply text-xs flex items-center;
+}
+
+.table__icon {
+  @apply w-3.5 h-3.5 text-gray-500 mr-1 lg:hidden
+}
+
+.task-action {
+  @apply text-sm text-black
+}
+
+@screen lg {
+  tr {
+    @apply table-row py-0;
+  }
+
+  .tableBody__cell {
+    @apply text-sm px-6 py-4 table-cell align-middle;
+  }
+
+  .task-action {
+    @apply text-gray-900;
+  }
+}
+</style>
