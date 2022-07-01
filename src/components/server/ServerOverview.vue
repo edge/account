1b9f3f6632260f7232536d139854a3bf41711874
Will Garrett <willgarrett64@yahoo.com>
2022-07-01 17:04:55 +0100

    feat: tweak layout of server overview tab

--- src/components/server/ServerOverview.vue
+++ src/components/server/ServerOverview.vue
@@ -181,23 +181,28 @@ export default {
     @apply grid-cols-2
   } */
 }
-
 @screen lg {
+  .server__specs .overview__grid {
+    @apply grid-cols-4
+  }
+}
+@screen xl {
   .server__details .overview__grid {
     @apply grid-cols-2
   }
-
   .server__specs .overview__grid {
-    @apply grid-cols-4
+    @apply grid-cols-2
   }
   .server__specs .label {
     @apply w-max;
   }
-}
-
-@screen xl {
   .server__costs .label {
     @apply w-max;
+  }
+}
+@screen 2xl {
+  .server__specs .overview__grid {
+    @apply grid-cols-4
   }
 }
 </style>
