85d5148cd5a485c70afd797a35087b274ff85d14
Will Garrett <willgarrett64@yahoo.com>
2022-07-01 11:55:39 +0100

    feat: change landing page background image

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,34 +3,27 @@
     <div class="landingPage__left">
       <router-view />
     </div>
-    <div class="landingPage__right">
-      <!-- eslint-disable-next-line max-len -->
-      <img src="https://images.unsplash.com/photo-1614777735430-7b46df56b404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=664&q=80"
-      class="object-cover w-full h-full"/>
-    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'LandingPage',
-  data() {
-    return {
-      accountNumber: ''
-    }
-  }
+  name: 'LandingPage'
 }
 </script>
 <style>
   .landingPage {
     @apply flex w-screen h-screen bg-white;
+    background-image: url("https://cdn.discordapp.com/attachments/976127390482849922/991745747651412078/unknown.png");
+    background-repeat: no-repeat;
+    background-size: cover;
   }
   .landingPage__left {
-    @apply flex justify-center items-start w-full h-full px-5 lg:w-1/2;
+    @apply flex justify-center items-start w-full h-full px-5 lg:w-1/2 z-10;
   }
   .landingPage__right {
-    @apply hidden w-1/2 h-full lg:block fixed right-0;
+    @apply absolute left-0 z-0;
   }
   .landingPage__content {
     @apply flex flex-col w-full my-auto space-y-8 py-10 sm:max-w-md;
