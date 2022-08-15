c298ddbdb4802df54033ca1f661e9d9ac475c317
Will <willgarrett64@yahoo.com>
2022-08-15 21:43:57 +0100

    fix: remove unnecessary check on server status

--- src/components/server/ServerDestroy.vue
+++ src/components/server/ServerDestroy.vue
@@ -86,13 +86,12 @@ export default {
       this.isLoading = true
       try {
         this.toggleConfirmationModal()
-        const deletingCrashedServer = this.server.status === 'crashed'
         const response = await utils.servers.deleteServer(
           process.env.VUE_APP_ACCOUNT_API_URL,
           this.session._key,
           this.serverId
         )
-        if (response.task || !deletingCrashedServer) this.$store.commit('addTask', response.task)
+        if (response.task) this.$store.commit('addTask', response.task)
         this.isLoading = false
       }
       catch (error) {
