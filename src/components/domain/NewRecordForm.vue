6fe988d6169c2a617e3ab7c1cc38b7ccc11b3d56
Will <willgarrett64@yahoo.com>
2022-08-30 18:16:36 +0100

    feat: change hostname input placeholder

--- src/components/domain/NewRecordForm.vue
+++ src/components/domain/NewRecordForm.vue
@@ -168,7 +168,7 @@ export default {
       }
       else return {
         label: 'Hostname',
-        placeholder: 'Enter hostname'
+        placeholder: 'Enter @ or hostname'
       }
     },
     types() {
