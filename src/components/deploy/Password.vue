760f528f439d74b86e3361a0382f4a8c090386d2
Will Garrett <willgarrett64@yahoo.com>
2022-09-08 17:36:21 +0100

    feat: resize buttons

--- src/components/deploy/Password.vue
+++ src/components/deploy/Password.vue
@@ -15,16 +15,16 @@
           :type="showPassword ? 'text' : 'password'"
           v-model="password"
         />
-        <EyeIcon v-if="showPassword" @click="showPassword = false" class="absolute right-0 w-5 h-5 text-gray-300 cursor-pointer" />
-        <EyeOffIcon v-if="!showPassword" @click="showPassword = true" class="absolute right-0 w-5 h-5 text-gray-300 cursor-pointer" />
+        <EyeIcon v-if="showPassword" @click="showPassword = false" class="absolute right-0 w-5 h-5 text-gray-400 hover:text-green cursor-pointer" />
+        <EyeOffIcon v-if="!showPassword" @click="showPassword = true" class="absolute right-0 w-5 h-5 text-gray-400 hover:text-green cursor-pointer" />
       </div>
     </div>
 
     <!-- buttons -->
-    <div class="flex mt-5 space-x-2 lg:mt-0">
-      <button @click.prevent="generate" class="button button--solid">Generate</button>
+    <div class="buttons__wrapper flex mt-5 space-x-2 lg:mt-0 justify-end">
+      <button @click.prevent="generate" class="button button--solid button--small">Generate</button>
       <button
-        class="button button--solid"
+        class="button button--solid button--small"
         v-if="canCopy"
         @click.prevent="copyToClipboard"
       >
@@ -85,4 +85,14 @@ export default {
 input.disabled {
   @apply cursor-not-allowed opacity-50;
 }
+
+@media (max-width: 400px) {
+  .buttons__wrapper {
+    @apply flex-col space-x-0 space-y-2;
+  }
+  .buttons__wrapper .button {
+    @apply w-full
+  }
+}
+
 </style>
