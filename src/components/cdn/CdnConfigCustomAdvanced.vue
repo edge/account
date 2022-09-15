f54e80a220343e17a7c7ed3c9f7af31a6c7d9068
Will Garrett <willgarrett64@yahoo.com>
2022-09-15 14:12:23 +0100

    feat: add json error feedback

--- src/components/cdn/CdnConfigCustomAdvanced.vue
+++ src/components/cdn/CdnConfigCustomAdvanced.vue
@@ -6,8 +6,24 @@
       @change=updateConfig
     >
     </textarea>
-    <div v-if="jsonError">
-      <span class="text-red">JSON invalid</span>
+    <!-- error -->
+    <div v-if="jsonError" class="flex flex-col text-red">
+      <span>JSON error around line {{ jsonError.lineNumber }}:</span>
+      <div>
+        <div v-if="jsonError.prevLineContent">
+          <span class="w-6 inline-block">{{ jsonError.lineNumber - 1 }}: </span>
+          <span class="monospace">{{ jsonError.prevLineContent }}</span>
+        </div>
+        <div>
+          <span class="w-6 inline-block">{{ jsonError.lineNumber }}: </span>
+          <span class="monospace">{{ jsonError.lineContent }}</span>
+        </div>
+
+        <div v-if="jsonError.nextLineContent">
+          <span class="w-6 inline-block">{{ jsonError.lineNumber + 1 }}: </span>
+          <span class="monospace">{{ jsonError.nextLineContent }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,7 +34,7 @@ export default {
   props: ['config'],
   data() {
     return {
-      jsonError: false
+      jsonError: null
     }
   },
   methods: {
@@ -26,12 +42,32 @@ export default {
       const json = e.target.value
       try {
         const config = JSON.parse(json)
-        this.jsonError = false
+        this.jsonError = null
         this.$emit('update-config', config)
 
       }
       catch (error) {
-        this.jsonError = true
+        // get where error occurs
+        const splitError = error.toString().split(' ')
+        const position = Number(splitError[splitError.findIndex(word => word === 'position') + 1])
+
+        // get lineNumber
+        const subStr = json.substring(0, position)
+        const lineNumber = subStr.split('\n').length
+        // get content of lines around where error occurs
+        let lineContent = json.split('\n')[lineNumber - 1]
+        if (lineContent) lineContent = lineContent.trim()
+        let prevLineContent = json.split('\n')[lineNumber - 2]
+        if (prevLineContent) prevLineContent = prevLineContent.trim()
+        let nextLineContent = json.split('\n')[lineNumber]
+        if (nextLineContent) nextLineContent = nextLineContent.trim()
+
+        this.jsonError = {
+          lineNumber,
+          lineContent,
+          prevLineContent,
+          nextLineContent
+        }
       }
     }
   },
