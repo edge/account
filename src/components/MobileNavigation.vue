85c7231cc28e5ddae2f7405d2836b65247724f50
Will Garrett <willgarrett64@yahoo.com>
2022-10-28 13:49:42 +0100

    feat: move support link in mobile menu to bottom links

--- src/components/MobileNavigation.vue
+++ src/components/MobileNavigation.vue
@@ -58,11 +58,6 @@ export default {
           link: '/shield',
           text: 'Shield',
           disabled: true
-        },
-        {
-          link: '/support',
-          text: 'Support',
-          disabled: false
         }
       ]
     }
