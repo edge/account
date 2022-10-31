2d376eb77c41e787e629b73e10c10e39a4539fd5
Will Garrett <willgarrett64@yahoo.com>
2022-10-31 12:28:36 +0000

    fix: hide balance warning message if have funds

--- src/components/confirmations/SubscriptionConfirmation.vue
+++ src/components/confirmations/SubscriptionConfirmation.vue
@@ -4,15 +4,15 @@
       <span class="text-green">Confirm subscription</span>
     </template>
     <template v-slot:body>
-      <div v-if="!balanceWarning" class="flex flex-col space-y-2">
+      <div v-if="balanceWarning">
+        <!-- eslint-disable-next-line max-len -->
+        <span>You need at least ${{ balance.threshold.warning.usd }} to upgrade to Priority Support. Please add funds to your account and try again.</span>
+      </div>
+      <div v-else class="flex flex-col space-y-2">
         <span>You will be billed daily for this support plan.</span>
         <span v-if="product.minDuration">
           There is a minimum term of {{ product.minDuration / 24 }} days before you can cancel this plan.
         </span>
-      </div>
-      <div>
-        <!-- eslint-disable-next-line max-len -->
-        <span>You need at least ${{ balance.threshold.warning.usd }} to upgrade to Priority Support. Please add funds to your account and try again.</span>
       </div>
     </template>
     <template v-slot:buttons>
