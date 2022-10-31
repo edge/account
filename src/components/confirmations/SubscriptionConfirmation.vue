30830a758fe5003cc61cd81b7a727f269ad7ca4a
Will Garrett <willgarrett64@yahoo.com>
2022-10-31 11:53:01 +0000

    fix: misc priority support fixes

--- src/components/confirmations/SubscriptionConfirmation.vue
+++ src/components/confirmations/SubscriptionConfirmation.vue
@@ -4,17 +4,22 @@
       <span class="text-green">Confirm subscription</span>
     </template>
     <template v-slot:body>
-      <div class="flex flex-col space-y-2">
-        You will be billed daily for this support plan.
+      <div v-if="hasFunds" class="flex flex-col space-y-2">
+        <span>You will be billed daily for this support plan.</span>
         <span v-if="product.minDuration">
           There is a minimum term of {{ product.minDuration / 24 }} days before you can cancel this plan.
         </span>
+      </div>
+      <div>
+        <!-- eslint-disable-next-line max-len -->
+        <span>You need at least ${{ balance.threshold.warning.usd }} to upgrade to Priority Support. Please add funds to your account and try again.</span>
       </div>
     </template>
     <template v-slot:buttons>
       <button
         class="w-full button button--small button--success"
         @click="confirm"
+        :disabled="balanceWarning"
       >
         Confirm
       </button>
@@ -30,12 +35,17 @@
 
 <script>
 import Modal from '@/components/Modal'
+import { mapGetters, mapState } from 'vuex'
 
 export default {
   name: 'SubscriptionConfirmation',
   props: ['product'],
   components: {
     Modal
+  },
+  computed: {
+    ...mapGetters(['balanceWarning']),
+    ...mapState(['balance'])
   },
   methods: {
     close() {
