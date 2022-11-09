9c16d0c55d759ca905f305166b2eccfa8fdb2172
Will Garrett <willgarrett64@yahoo.com>
2022-11-09 11:01:45 +0000

    feat: add icons

---
+++ src/components/icons/ShiftIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- eslint-disable max-len-->
+  <svg width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="shiftIconTitle" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" color="#000000"><path d="M5,21 L19,21 L5,21 Z M16,12 L16,17 L8,17 L8,12 L3,12 L12,3 L21,12 L16,12 Z"/> </svg>
+  <!-- eslint-enable max-len-->
+</template>
+
+<script>
+export default {
+  name: 'ShiftIcon'
+}
+</script>
