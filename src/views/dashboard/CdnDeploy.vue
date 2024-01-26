5b8cd905d9da63dea025623e454f72f37dabc917
Aneurin Barker Snook <a@aneur.in>
2024-01-26 13:49:45 +0000

    refactor: patch integration api usage

--- src/views/dashboard/CdnDeploy.vue
+++ src/views/dashboard/CdnDeploy.vue
@@ -225,7 +225,7 @@ export default {
         const { integration } = await api.integration.addIntegration(
           process.env.VUE_APP_ACCOUNT_API_URL,
           this.session._key,
-          this.integration
+          { ...this.integration, account: this.account._key }
         )
         this.deployedIntegration = integration
 
