cfcbe149aa1c617dacb73bf6ab4250534a73cc8f
Aneurin Barker Snook <aneurin@aneur.in>
2025-07-15 16:36:17 +0100

    fix: deployment warning text

--- src/components/DeploymentWarning.vue
+++ src/components/DeploymentWarning.vue
@@ -1,8 +1,14 @@
 <template>
   <div :class="`box flex space-x-2 ${className || ''}`">
     <div><ExclamationIcon class="w-5 text-red" /></div>
-    <span v-if="account.useCryptoView" class="text-red">Deployments are disabled while your balance is below ${{ balance.threshold.warning.usd }}. Please add funds or set up Pay by Credit Card to deploy new services.</span>
-    <span v-else class="text-red">Deployments are disabled until a payment card has been added to your account.</span>
+    <span v-if="account.useCryptoView" class="text-red">
+      <span v-if="config.accounts.limits.ignoreBalanceRestrictionsWithCard">Deployments are disabled while your balance is below ${{ balance.threshold.warning.usd }}. Please add funds or set up Pay by Credit Card to deploy new services.</span>
+      <span v-else>Deployments are disabled while your balance is below ${{ balance.threshold.warning.usd }}. Please add funds to enable services.</span>
+    </span>
+    <span v-else class="text-red">
+      <span v-if="config.accounts.limits.ignoreBalanceRestrictionsWithCard">Deployments are disabled until a payment card has been added to your account.</span>
+      <span v-else>Deployments are disabled while your balance is below ${{ balance.threshold.warning.usd }}. Please add funds to enable services.</span>
+    </span>
   </div>
 </template>
 
@@ -13,7 +19,7 @@ import { mapState } from 'vuex'
 export default {
   name: 'DeploymentWarning',
   computed: {
-    ...mapState(['account'])
+    ...mapState(['account', 'balance', 'config'])
   },
   props: ['className'],
   components: { ExclamationIcon }
